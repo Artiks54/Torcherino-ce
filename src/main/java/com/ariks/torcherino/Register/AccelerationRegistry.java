659c411eba53d3c0651f5c23eaf77538f506f78d
Artiks54 <novickoff.artiom2016@yandex.ru>
2024-02-07 18:22:29 +0300

    update 7-6-9.1

--- src/main/java/com/ariks/torcherino/Register/AccelerationRegistry.java
+++ src/main/java/com/ariks/torcherino/Register/AccelerationRegistry.java
@@ -1,4 +1,5 @@
 package com.ariks.torcherino.Register;
+
 import java.util.HashSet;
 import java.util.Set;
 import com.ariks.torcherino.Torcherino;
@@ -6,45 +7,47 @@ import net.minecraft.block.Block;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 
-public class AccelerationRegistry
-{
-	public static void blacklistString(String string)
-	{
-		if (string.indexOf(':') == -1) {
-			try
-			{
-		        Class<?> clazz = Torcherino.instance.getClass().getClassLoader().loadClass(string);
-		        if(clazz == null) {
-		        	Torcherino.logger.info("Class null: " + string);
-		            return;
-		        }
-		        else if(!TileEntity.class.isAssignableFrom(clazz)) {
-		        	Torcherino.logger.info("Class not a TileEntity: " + string);
-		            return;
-		        }
-		        blacklistTile((Class<? extends TileEntity>) clazz);
-		    }
-			catch(ClassNotFoundException e) {
+public class AccelerationRegistry {
+	private static final Set<Block> blacklistedBlocks = new HashSet<>();
+	private static final Set<Class<? extends TileEntity>> blacklistedTiles = new HashSet<>();
+
+	public static void blacklistString(String string) {
+		if (!string.contains(":")) {
+			try {
+				Class<?> clazz = Torcherino.instance.getClass().getClassLoader().loadClass(string);
+				if (clazz == null) {
+					Torcherino.logger.info("Class null: " + string);
+					return;
+				} else if (!TileEntity.class.isAssignableFrom(clazz)) {
+					Torcherino.logger.info("Class not a TileEntity: " + string);
+					return;
+				}
+				blacklistTile(clazz.asSubclass(TileEntity.class));
+			} catch (ClassNotFoundException e) {
 				Torcherino.logger.info("Class not found: " + string + ", ignoring");
-		    }
+			}
 		} else {
 			String[] parts = string.split(":");
-		    if(parts.length != 2) {
-		    	Torcherino.logger.info("Received malformed message: " + string);
-		    	return;
+			if (parts.length != 2) {
+				Torcherino.logger.info("Received malformed message: " + string);
+				return;
 			}
-		    Block block = Block.REGISTRY.getObject(new ResourceLocation(parts[0], parts[1]));
-            Torcherino.logger.info("Blacklisting block: " + block.getUnlocalizedName());
-		    blacklistBlock(block);
+			ResourceLocation location = new ResourceLocation(parts[0], parts[1]);
+			Block block = Block.REGISTRY.getObject(location);
+			Torcherino.logger.info("Blacklisting block: " + block.getUnlocalizedName());
+			blacklistBlock(block);
 		}
 	}
-    public static void blacklistBlock(Block block)
-    {
-        AccelerationRegistry.blacklistedBlocks.add(block);
-    }
-    public static void blacklistTile(Class<? extends TileEntity> tile) {AccelerationRegistry.blacklistedTiles.add(tile);}
-    public static boolean isBlockBlacklisted(Block block) {return AccelerationRegistry.blacklistedBlocks.contains(block);}
-    public static boolean isTileBlacklisted(Class<? extends TileEntity> tile) {return AccelerationRegistry.blacklistedTiles.contains(tile);}
-    private static final Set<Block> blacklistedBlocks = new HashSet<Block>();
-    private static final Set<Class<? extends TileEntity>> blacklistedTiles = new HashSet<Class<? extends TileEntity>>();
+	public static void blacklistBlock(Block block) {
+		blacklistedBlocks.add(block);
+	}
+	public static void blacklistTile(Class<? extends TileEntity> tile) {
+		blacklistedTiles.add(tile);
+	}
+	public static boolean isBlockBlacklisted(Block block) {
+		return blacklistedBlocks.contains(block);
+	}
+	public static boolean isTileBlacklisted(Class<? extends TileEntity> tile) {
+		return blacklistedTiles.contains(tile);
+	}
 }
