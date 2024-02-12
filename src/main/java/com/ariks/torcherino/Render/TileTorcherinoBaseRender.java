96132d20fb0b4f8a7dbecf2d7b3c4c9765a53bf6
Artiks54 <novickoff.artiom2016@yandex.ru>
2024-02-12 09:43:52 +0300

    update 7-7 render

---
+++ src/main/java/com/ariks/torcherino/Render/TileTorcherinoBaseRender.java
@@ -0,0 +1,37 @@
+package com.ariks.torcherino.Render;
+
+import com.ariks.torcherino.Tiles.TileTorcherinoBase;
+import net.minecraft.client.renderer.*;
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
+public class TileTorcherinoBaseRender extends TileEntitySpecialRenderer<TileTorcherinoBase> {
+    @Override
+    public void render(TileTorcherinoBase tile, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
+        if (!tile.hasWorld() || !tile.booleanRender)
+            return;
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(x - 0.0005D, y - 0.0005D, z - 0.0005D);
+        GlStateManager.scale(0.999D, 0.999D, 0.999D);
+        GlStateManager.depthMask(false);
+        GlStateManager.enableBlend();
+        GlStateManager.disableTexture2D();
+        GlStateManager.disableLighting();
+        GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE);
+        GlStateManager.disableCull();
+        int i = 61680;
+        int j = i % 65536;
+        int k = 0;
+        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float) j, (float) k);
+        RenderGlobal.renderFilledBox(tile.getAABBForRender(), 1F, 1F, 1F, 0.75F);
+        RenderGlobal.drawSelectionBoundingBox(tile.getAABBForRender(), 1F, 1F, 0F, 1F);
+        GlStateManager.enableCull();
+        GlStateManager.enableLighting();
+        GlStateManager.enableTexture2D();
+        GlStateManager.disableBlend();
+        GlStateManager.depthMask(true);
+        GlStateManager.popMatrix();
+    }
+}
