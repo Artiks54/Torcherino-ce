1d24e5ac4af52a45d7d1314bb26bf2a8e23d1e1b
Artiks54 <novickoff.artiom2016@yandex.ru>
2024-05-10 11:35:25 +0300

    update 7-7-7

--- src/main/java/com/ariks/torcherino/Block/ParticleCollector/BlockParticleCollector.java
+++ src/main/java/com/ariks/torcherino/Block/ParticleCollector/BlockParticleCollector.java
@@ -1,15 +1,10 @@
 package com.ariks.torcherino.Block.ParticleCollector;
 
 import com.ariks.torcherino.Block.ExmapleBlock;
-import com.ariks.torcherino.Register.RegistryGui;
-import com.ariks.torcherino.Torcherino;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -32,14 +27,6 @@ public class BlockParticleCollector extends ExmapleBlock {
         assert tileEntity != null;
         InventoryHelper.dropInventoryItems(worldIn, pos, (IInventory) tileEntity);
         super.breakBlock(worldIn, pos, state);
-    }
-    @Override
-    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-        TileEntity tile = worldIn.getTileEntity(pos);
-        if (!worldIn.isRemote && tile instanceof TileParticleCollector) {
-            playerIn.openGui(Torcherino.instance, RegistryGui.GUI_PARTICLE_COLLECTOR, worldIn, pos.getX(), pos.getY(), pos.getZ());
-        }
-        return true;
     }
     @Override
     public TileEntity createTileEntity(@NotNull World world, @NotNull IBlockState state) {
--- src/main/java/com/ariks/torcherino/integration/Jei/AddonJei.java
+++ src/main/java/com/ariks/torcherino/integration/Jei/AddonJei.java
@@ -1,12 +1,22 @@
 package com.ariks.torcherino.integration.Jei;
 
+import com.ariks.torcherino.Register.RegistryBlock;
+import com.ariks.torcherino.Register.RegistryItems;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
 import mezz.jei.api.JEIPlugin;
-import org.jetbrains.annotations.NotNull;
+import net.minecraft.item.ItemStack;
 
 @JEIPlugin
 public class AddonJei implements IModPlugin {
-    public void register(@NotNull IModRegistry registry) {
+    public void register(IModRegistry registry) {
+        registry.addIngredientInfo(new ItemStack(RegistryItems.time_particle), ItemStack.class, "Generated in a particle collector.");
+        registry.addIngredientInfo(new ItemStack(RegistryBlock.Particle_collectors_lvl1), ItemStack.class, "Generates time particles * 1");
+        registry.addIngredientInfo(new ItemStack(RegistryBlock.Particle_collectors_lvl2), ItemStack.class, "Generates time particles * 8");
+        registry.addIngredientInfo(new ItemStack(RegistryBlock.Particle_collectors_lvl3), ItemStack.class, "Generates time particles * 64");
+        registry.addIngredientInfo(new ItemStack(RegistryBlock.Time_Manipulator), ItemStack.class, "Allows you to change the time in the world.");
+        registry.addIngredientInfo(new ItemStack(RegistryBlock.Time_Acceleration), ItemStack.class, "Uses time to accelerate by 500% within a 3x3x3 radius");
+        registry.addIngredientInfo(new ItemStack(RegistryBlock.Time_collectors), ItemStack.class, "Passively accumulates time");
+        registry.addIngredientInfo(new ItemStack(RegistryBlock.Time_Storage), ItemStack.class, "It is used to store time");
     }
 }
