325a6a1ed3793b0d99b00211e3950c17c568e2c6
Artiks54 <novickoff.artiom2016@yandex.ru>
2024-05-17 08:50:18 +0300

    update 7-7-8

--- src/main/java/com/ariks/torcherino/Torcherino.java
+++ src/main/java/com/ariks/torcherino/Torcherino.java
@@ -14,7 +14,7 @@ import net.minecraftforge.fml.common.event.FMLInterModComms;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Torcherino.MOD_ID, name = Torcherino.MOD_NAME, useMetadata = true, acceptedMinecraftVersions = "[1.12]", version = Torcherino.VERSION,updateJSON = "https://github.com/Artiks54/Torcherino-ce/update.json")
+@Mod(modid = Torcherino.MOD_ID, name = Torcherino.MOD_NAME, useMetadata = true, acceptedMinecraftVersions = "[1.12]", version = Torcherino.VERSION)
 public class Torcherino {
 	public static File config;
 	public static CreativeTabs torcherinoTab = new TorchTab("torcherinoTab");
